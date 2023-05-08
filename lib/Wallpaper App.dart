7e956cbf0ebd05403847e75d9d5bd2ad28dbf8b2
HirenDube <heena.dube091@gmail.com>
2023-05-08 16:01:48 +0530

    Wallpaper Saver App - Flutter Previous blank screen errors solved

--- lib/Wallpaper App.dart
+++ lib/Wallpaper App.dart
@@ -14,7 +14,7 @@ class MyWallpaperApp extends StatefulWidget {
 }
 
 class _MyWallpaperAppState extends State<MyWallpaperApp> {
-  String pixelsAPIKey =
+  String pixelsApiKey =
       "cKLXs3wd22ttfeHO8Mc0nIflsOKaaA4lKGyMj8rw6Em2UtYZ6QZpaW1w";
   List imageData = [];
   int page = 1;
